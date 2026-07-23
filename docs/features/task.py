ee673434c239123aa573b56dbcb47f767e8629d3
Alina <alina@example.com>
2026-07-23 17:30:46 +0500

    Add remove_task function

--- docs/features/task.py
+++ docs/features/task.py
@@ -3,3 +3,10 @@ tasks = []
 def add_task(task):
     tasks.append(task)
     print(f"Задача '{task}' добавлена.")
+
+def remove_task(index):
+    if 0 <= index < len(tasks):
+        removed = tasks.pop(index)
+        print(f"Задача '{removed}' удалена.")
+    else:
+        print("Неверный индекс.")
